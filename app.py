d26bc1249ae200c652947790b939fb47ce5f5711
obadelej <obadelej@gmail.com>
2024-07-26 14:30:58 +0000

    Added navbar and requirements.txt

--- app.py
+++ app.py
@@ -3,9 +3,45 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ATHLETES =[
+    {
+        'id': 1,
+        'first_name': "Shantay",
+        'last_name': "Augustine",
+        'birthdate': '30/11/2006',
+        'gender':'Female'
+    },
+    {
+        'id': 2,
+        'first_name': "Ethan",
+        'last_name': "Sam",
+        'birthdate': '11/01/2007',
+        'gender': 'Male'
+    },
+    {
+        'id': 3,
+        'first_name': "Devonni",
+        'last_name': "Ferguson",
+        'birthdate': '17/10/2005',
+        'gender': 'Male'
+    },
+    {
+        'id': 4,
+        'first_name': "Rhea",
+        'last_name': "Flanders",
+        'birthdate': '21/08/2004',
+        'gender': 'Female'
+    }    
+]
+
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
+@app.route('/athletes')
+def athletes_page():
+    return render_template('athletes.html', athletes=ATHLETES)
+
     
 
 if __name__ == '__main__':
